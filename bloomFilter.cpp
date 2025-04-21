b6c01074eaca27f8fb0ea662df917a54f234defb
Matan Shaul <matan.shaul@live.biu.ac.il>
2025-04-21 10:44:06 +0300

    chasnge the bloom filter to make it shorter

--- bloomFilter.cpp
+++ bloomFilter.cpp
@@ -1,40 +1,41 @@
 #include <vector>
 #include <string>
-#include"bloomFilter.h"
+#include "bloomFilter.h"
 #include <functional> // For std::hash
-using namespace std;
-class bloomFilter{
-private:
-    int my_size;
-    vector<function<int(const string&)>> my_hashFunctions; // Vector of hash functions
-    vector<bool> bitArray; // Bit array to represent the bloom filter
-public:
-    // 1. Constructor: Initializes the bloom filter with a given size and number of hash functions.
-    bloomFilter(int size, const vector<function<int(const string&)>>& hashFunctions) {
-        my_size = size;
-        my_hashFunctions = hashFunctions;
-        bitArray.resize(size, false); // Initialize the bit array with false values
-    }
-    // 2. Destructor: Cleans up any allocated resources.
-    ~bloomFilter(){}
-    // 3. Add: Adds an URL to the bloom filter.
-    void add(const string &URL){
-        // Hash the URL and set the corresponding bits in the bit array
-        for (const auto& hashFunction : my_hashFunctions) {
-            int hashValue = hashFunction(URL) % my_size;
-            bitArray[hashValue] = true;
-        }
-    }
-    // 4. Contains: Checks if an URL is possibly in the bloom filter.
-    bool contains(const string &URL) {
-        // Check if all the bits corresponding to the hashes are set to true
-        for (const auto& hashFunction : my_hashFunctions) {
-            int hashValue = hashFunction(URL) % my_size;
-            if (!bitArray[hashValue]) {
-                return false; // If any bit is false, the URL is definitely not in the filter
-            }
-        }
-        return true; // If all bits are true, the URL is possibly in the filter
-    }  
 
-};
+using namespace std;
+
+// Hash function implementation
+int bloomFilter::hash(const string& key, int seed) const {
+    // Simple hash function implementation using std::hash with a seed
+    std::hash<string> hasher;
+    unsigned int hashValue = hasher(key);
+    return (hashValue ^ seed) % bitArray.size();
+}
+
+// Constructor implementation
+bloomFilter::bloomFilter(int size, int numHashes) {
+    bitArray.resize(size, false); // Initialize the bit array with false values
+    numHashFunctions = numHashes; // Set the number of hash functions
+}
+
+// Add implementation
+void bloomFilter::add(const string& key) {
+    // Hash the key and set the corresponding bits in the bit array
+    for (int i = 0; i < numHashFunctions; i++) {
+        int hashValue = hash(key, i);
+        bitArray[hashValue] = true;
+    }
+}
+
+// Contains implementation
+bool bloomFilter::contains(const string& key) const {
+    // Check if all the bits corresponding to the hashes are set to true
+    for (int i = 0; i < numHashFunctions; i++) {
+        int hashValue = hash(key, i);
+        if (!bitArray[hashValue]) {
+            return false; // If any bit is false, the key is definitely not in the filter
+        }
+    }
+    return true; // If all bits are true, the key is possibly in the filter
+}
--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main(){
    {
    int arraySize;
     int numHashes;
-    cin >> arraySize, numHashes; // Read the size of the bloom filter and the number of hash functions
+    cin >> arraySize >> numHashes; // Read the size of the bloom filter and the number of hash functions
     bloomFilter bf(arraySize, numHashes); // Create a bloom filter with the specified size and number of hash functions
     string url;
     int choice;
-    cin >> choice, url; // Read the user's choice and the URL
+    cin >> choice >> url; // Read the user's choice and the URL
     switch (choice) {
         case 1: // Add URL
             URL1(bf, url); // Add the URL to the bloom filter
@@ -20,6 +20,9 @@ int main(){
             } else {
                 cout << "URL is definitely not in the bloom filter." << endl;
             }
+            break;
+        default: // Invalid choice
+            cout << "Invalid choice. Please enter 1 or 2." << endl;
             break;
     }
    }
