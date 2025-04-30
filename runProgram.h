a137d2932346df5824e77c6cafdd6f9e653ed621
ilaip100 <ilai.pingle@live.biu.ac.il>
2025-04-30 19:43:54 +0300

    Remove unused bloom filter tests and related files; delete run program implementation

--- runProgram.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-class runProgram{
-public:
-    runProgram();
-    void run();
-    void setProgramName(std::string name);
-    std::string getProgramName();
-    ~runProgram();
-};
